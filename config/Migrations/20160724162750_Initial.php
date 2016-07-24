4673f09c051982026fe23c1e53155208735c5ae9
SK <sylwester.kowal@21w.pl>
2016-07-24 18:28:56 +0200

    Install files

---
+++ config/Migrations/20160724162750_Initial.php
@@ -0,0 +1,67 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    public function up()
+    {
+
+        $this->table('redirects')
+            ->addColumn('plugin', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('controller', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('action', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('pass', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('slug', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'slug',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'plugin',
+                    'controller',
+                    'action',
+                    'pass',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'slug',
+                ]
+            )
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('redirects');
+    }
+}
